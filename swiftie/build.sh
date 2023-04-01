bb55c823f6b6ef1ec212f955706e10a158c97487
uvlad7 <uvlad7@gmail.com>
2023-04-01 05:41:41 +0300

    Note about header files in swift

--- swiftie/build.sh
+++ swiftie/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # swift package resolve && swiftc Sources/swiftie/swiftie.swift -emit-module -emit-library
-# swift package resolve && swiftc Sources/swiftie/swiftie.swift -emit-library
+# swift package resolve && swiftc Sources/swiftie/swiftie.swift -emit-library -emit-objc-header-path swiftie.h
 swift build && cp .build/debug/libswiftie.so .
