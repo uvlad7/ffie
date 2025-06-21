f98e00c4914cc7c8b2c8368478055b825f00c8a1
uvlad7 <uvlad7@gmail.com>
2025-06-22 02:22:28 +0300

    add csharp atexit

--- sharpie/Sharpie.cs
+++ sharpie/Sharpie.cs
@@ -9,11 +9,24 @@ namespace sharpie;
 // $ dotnet new create classlib --output . && mv Class1.cs Sharpie.cs
 public static class Sharpie
 {
+    public static event EventHandler? ProcessExit;
+
     [UnmanagedCallersOnly(EntryPoint = "hello_sharpie", CallConvs = new[] { typeof(CallConvCdecl) })]
     public static void HelloSharpie() {
         // Ignore NO_COLOR and other envs
         Pastel.ConsoleExtensions.Enable();
         // Color - right triangle of the logo
         Console.WriteLine($"Hello from {"C#".Pastel(Color.FromArgb(129, 44, 151))}!");
+        // AppDomain.CurrentDomain.ProcessExit and DomainUnload don't work out of the box
+        // and I failed to trigger them manually, so here is a custom solution
+        ProcessExit += (_, _) => 
+        {
+            Console.WriteLine($"Bye from {"C#".Pastel(Color.FromArgb(129, 44, 151))}!");
+        };
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "exit_csharp", CallConvs = new[] { typeof(CallConvCdecl) })]
+    public static void ExitCSharp() {
+        ProcessExit?.Invoke(null, EventArgs.Empty);
     }
 }
