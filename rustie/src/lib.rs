cf92b0385a4ea664c38fc919b62b8cf90bb96e93
uvlad7 <uvlad7@gmail.com>
2025-06-22 02:40:11 +0300

    add rust atexit, ref

--- rustie/src/lib.rs
+++ rustie/src/lib.rs
@@ -1,11 +1,18 @@
 extern crate ansi_term;
 use ansi_term::Colour;
 
+extern "C" fn bye_rustie() {
+    println!("Bye from {}!", Colour::RGB(227, 58, 37).paint("Rust"));
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn hello_rustie() {
-    println!("Hello from {}!",
-             // Colour::Yellow.paint("Rust"));
-             // Colour::RGB(183, 65, 14).paint("Rust"));
-             // Colour::RGB(246, 76, 0).paint("Rust"));
-             Colour::RGB(227, 58, 37).paint("Rust"));
+    println!(
+        "Hello from {}!",
+        // Colour::Yellow.paint("Rust"));
+        // Colour::RGB(183, 65, 14).paint("Rust"));
+        // Colour::RGB(246, 76, 0).paint("Rust"));
+        Colour::RGB(227, 58, 37).paint("Rust")
+    );
+    libc::atexit(bye_rustie);
 }
