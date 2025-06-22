cff6bef1d5b8840530067cab804adfc2c54eee71
uvlad7 <uvlad7@gmail.com>
2025-06-22 07:26:35 +0300

    llvmruby atexit, dispose

--- llrubie/llrubie.rb
+++ llrubie/llrubie.rb
@@ -14,6 +14,7 @@ module LlRubie
     mod = LLVM::Module.new('hello')
     # hello_str = LLVM::ConstantArray.string("Hello from #{Paint['Ruby', [136, 17, 2]]}#{Paint['LLVM', [43, 97, 122]]}!")
     hello_str = LLVM::ConstantArray.string("Hello from #{Rainbow('Ruby').color(136, 17, 2)}#{Rainbow('LLVM').color(43, 97, 122)}!")
+    bye_str = LLVM::ConstantArray.string("Bye from #{Rainbow('Ruby').color(136, 17, 2)}#{Rainbow('LLVM').color(43, 97, 122)}!")
 
     # Declare the string constant as a global constant.
     hello = mod.globals.add(hello_str, :hello) do |var|
@@ -23,11 +24,36 @@ module LlRubie
       var.initializer = hello_str
     end
 
+    bye = mod.globals.add(bye_str, :bye) do |var|
+      var.linkage = :private
+      var.global_constant = true
+      var.unnamed_addr = true
+      var.initializer = bye_str
+    end
+
     # External declaration of the `puts` function
     cputs = mod.functions.add('puts', [LLVM.Pointer(LLVM::Int8)], LLVM::Int32) do |function, string|
       function.add_attribute :no_unwind_attribute
       string.add_attribute :no_capture_attribute
       string.add_attribute :readonly_attribute
+    end
+
+    # External declaration of the `atexit` function
+    catexit = mod.functions.add('atexit', [LLVM.Pointer(LLVM.Function([], LLVM.Void))], LLVM::Int32) do |function, fptr|
+      function.add_attribute :no_unwind_attribute
+      fptr.add_attribute :no_capture_attribute
+      fptr.add_attribute :readonly_attribute
+    end
+
+    # Definition of bye_llrubie function
+    bye_llrubie_fn = mod.functions.add('bye_llrubie', [], LLVM.Void) do |function|
+      function.basic_blocks.append.build do |b|
+        zero = LLVM.Int(0) # a LLVM Constant value
+        cast210 = b.gep bye, [zero, zero], 'cast210'
+        # Call puts function to write out the string to stdout.
+        b.call cputs, cast210
+        b.ret nil
+      end
     end
 
     # Definition of hello_llrubie function
@@ -42,13 +68,17 @@ module LlRubie
         cast210 = b.gep hello, [zero, zero], 'cast210'
         # Call puts function to write out the string to stdout.
         b.call cputs, cast210
+
+        b.call catexit, bye_llrubie_fn
+
         b.ret zero
       end
     end
     LLVM.init_jit
 
     engine = LLVM::JITCompiler.new(mod, opt_level: 3)
-    engine.run_function(hello_llrubie_fn)
-    engine.dispose
+    engine.run_function(hello_llrubie_fn).dispose
+    # Don't dispose because of atexit
+    # engine.dispose
   end
 end
