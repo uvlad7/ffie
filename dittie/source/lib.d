3068ed427962f3b2200fab8c4994ebfdeb268fc0
uvlad7 <uvlad7@gmail.com>
2025-06-22 06:23:53 +0300

    add dlang atexit

--- dittie/source/lib.d
+++ dittie/source/lib.d
@@ -3,8 +3,17 @@ import terminal;
 // 		"console-colors": "~>1.1.0",
 // "targetType": "dynamicLibrary"
 
+import core.stdc.stdlib;
+
+extern (C) void bye_dittie()
+{
+    auto terminal = new Terminal();
+    terminal.writeln("Bye from ", Foreground(176, 57, 50), "D", Foreground(Color.reset), "!");
+}
+
 extern (C) void hello_dittie()
 {
 	auto terminal = new Terminal();
     terminal.writeln("Hello from ", Foreground(176, 57, 50), "D", Foreground(Color.reset), "!");
+    atexit(&bye_dittie);
 }
