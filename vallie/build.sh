935003074eee0e2b2e754942e98042cc5649f4e3
uvlad7 <uvlad7@gmail.com>
2025-10-21 03:08:25 +0300

    try to use gobject

--- vallie/build.sh
+++ vallie/build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 valac --pkg posix --library=vallie -H vallie.h vallie.vala -X -fPIC -X -shared -o libvallie.so
+# valac --pkg posix --library=vallie --gir vallie-0.1.gir -H vallie.h vallie.vala -X -fPIC -X -shared -o libvallie.so && g-ir-compiler --shared-library=libvallie.so --output=vallie-0.1.typelib vallie-0.1.gir
