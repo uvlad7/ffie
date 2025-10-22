c051006eb380c22cc77a26877151763ef7c04743
uvlad7 <uvlad7@gmail.com>
2025-10-22 13:29:05 +0300

    fix realpath

--- gvallie/build.sh
+++ gvallie/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-valac --pkg posix --library=gvallie --gir gvallie-0.1.gir -H gvallie.h gvallie.vala -X -fPIC -X -shared -o libgvallie.so && g-ir-compiler --shared-library=$(realpath libgvallie.so) --output=gvallie-0.1.typelib gvallie-0.1.gir
+valac --pkg posix --library=gvallie --gir gvallie-0.1.gir -H gvallie.h gvallie.vala -X -fPIC -X -shared -o libgvallie.so && g-ir-compiler --shared-library=$(realpath -s libgvallie.so) --output=gvallie-0.1.typelib gvallie-0.1.gir
